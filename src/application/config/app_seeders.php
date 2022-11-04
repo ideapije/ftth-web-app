bc0238b2908a93c8eb99ab98b0e9e51587bffeb5
ifirmawan <firmawaneiwan@gmail.com>
2022-11-04 13:17:45 +0700

    test: ganti angka untuk testing

--- src/application/config/app_seeders.php
+++ src/application/config/app_seeders.php
@@ -1,13 +1,13 @@
 <?php
 
-$config['supplies'] = [7, 9, 18];
-$config['total'] = 34; // jumlah dari supplies
+$config['supplies'] = [40, 60, 50];
+$config['total'] = 150; // jumlah dari supplies
 
-$config['demands'] = [5, 8, 7, 14];
+$config['demands'] = [20, 30, 50, 50];
 
 $config['values'] = [
-    [19, 70, 40], // D1
-    [30, 30, 8], // D2
-    [50, 40, 70], // D3
-    [10, 60, 20], // d4
+    [4, 6, 5], // D1
+    [6, 8, 7], // D2
+    [8, 6, 6], // D3
+    [8, 7, 8], // d4
 ];
