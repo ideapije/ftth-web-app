e940a6857c014b97dd44b6faccf5ca2bc4a801e2
ifirmawan <firmawaneiwan@gmail.com>
2023-01-09 08:09:52 +0700

    chore: Add least_cost and MODI documentation

--- src/application/libraries/Least_cost.php
+++ src/application/libraries/Least_cost.php
@@ -2,20 +2,37 @@
 
 class Least_cost
 {
-    public function get_solution($costs, $supply, $demand)
+    /**
+     * Here is a general outline of how you could implement the least cost method in PHP:
+     * 1. Begin by representing the transportation problem as a matrix, with the rows representing the sources and the columns representing the destinations. The cells of the matrix should contain the cost of shipping one unit of goods from the corresponding source to the corresponding destination.
+     * 2. Initialize the solution matrix to all zeros. This matrix will store the final shipping amounts for each source-destination pair.
+     * 3. Calculate the net supply and net demand at each location. The net supply is the total amount of goods available at each source, and the net demand is the total amount of goods needed at each destination.
+     * 4. While there is still unbalanced supply or demand:
+     *     4.1 Find the location with the largest excess supply or demand (whichever is greater in magnitude).
+     *     4.2 If the excess supply is greater, find the location with the smallest cost for shipping to a location with excess demand.
+     *     4.3 If the excess demand is greater, find the location with the smallest cost for shipping from a location with excess supply.
+     *     4.4 Ship the maximum amount possible from the excess supply location to the excess demand location, and update the solution matrix and the net supply and demand values accordingly.
+     * 5. When there is no more unbalanced supply or demand, the solution matrix contains the optimal shipping amounts for the unbalanced transportation problem.
+     */
+    public function least_cost_solution($costs, $supply, $demand)
     {
+        // 2. Initialize the solution matrix to all zeros
         $solution = array_fill(0, count($supply), array_fill(0, count($demand), 0));
+        // 3. Calculate the net supply and net demand at each location
         $netSupply = $supply;
         $netDemand = $demand;
-
+         // 4. While there is still unbalanced supply or demand
         while (array_sum($netSupply) != 0 || array_sum($netDemand) != 0) {
+            // Find the location with the largest excess supply or demand (whichever is greater in magnitude)
             $maxExcess = max(array_merge([0], array_map(function ($x) {
                 return abs($x);
             }, array_merge($netSupply, $netDemand))));
             $maxExcessIndex = array_search($maxExcess, $netSupply) !== false ? array_search($maxExcess, $netSupply) : array_search($maxExcess, $netDemand) + count($supply);
             $isSupply = $maxExcessIndex < count($supply);
-
+            
+            
             if ($isSupply) {
+                // 4.1 Find the location with the smallest cost for shipping to a location with excess demand
                 $minCost = INF;
                 $minCostIndex = -1;
                 for ($i = 0; $i < count($demand); $i++) {
@@ -24,11 +41,13 @@ class Least_cost
                         $minCostIndex = $i;
                     }
                 }
+                // Ship the maximum amount possible from the excess supply location to the excess demand location
                 $shippingAmount = min($maxExcess, $netDemand[$minCostIndex]);
                 $solution[$maxExcessIndex][$minCostIndex] += $shippingAmount;
                 $netSupply[$maxExcessIndex] -= $shippingAmount;
                 $netDemand[$minCostIndex] -= $shippingAmount;
             } else {
+                // 4.3 Find the location with the smallest cost for shipping from a location with excess supply
                 $minCost = INF;
                 $minCostIndex = -1;
                 for ($i = 0; $i < count($supply); $i++) {
@@ -37,6 +56,7 @@ class Least_cost
                         $minCostIndex = $i;
                     }
                 }
+                // Ship the maximum amount possible from the excess supply location to the excess demand location
                 $shippingAmount = min($maxExcess, $netSupply[$minCostIndex]);
                 $solution[$minCostIndex][$maxExcessIndex - count($supply)] += $shippingAmount;
                 $netSupply[$minCostIndex] -= $shippingAmount;
--- src/application/libraries/Modi_method.php
+++ src/application/libraries/Modi_method.php
@@ -2,6 +2,16 @@
 
 class Modi_method
 {
+    /**
+     * The following MODI steps to solve transportation problem : 
+     * 1. Begin with an initial feasible solution for the transportation problem. This can be found using the northwest corner rule, minimum cost method, or Vogel's approximation method.
+     * 2. Calculate the "opportunity costs" for each non-basic variable (i.e., each unused transportation route). This is done by adding the supply and demand values for that route to the transportation cost for that route.
+     * 3. Find the non-basic variable (unused transportation route) with the smallest opportunity cost. This is the route that will be used to modify the initial solution.
+     * 4. Check whether the demand at the destination of the chosen route is greater than or less than the supply at the source.
+     *    - If the demand is greater than the supply, allocate the full supply to the chosen route and reduce the demand at the destination accordingly.
+     *    - If the demand is less than the supply, allocate the full demand to the chosen route and reduce the supply at the source accordingly.
+     * 5. Repeat this process until no further improvements can be made. At this point, the solution is optimal.
+     */
     public $bfs_copy = [];
 
     public function get_uv($bfs, $cost_matrix, $default)
@@ -40,7 +50,7 @@ class Modi_method
     public function convert_values_to_integer($data)
     {
         $send = [];
-        foreach($data as $value) {
+        foreach ($data as $value) {
             $send[] = intval($value);
         }
         return $send;
@@ -58,5 +68,69 @@ class Modi_method
             $send[] = $rows;
         }
         return $send;
+    }
+
+    public function modi_solution($costs, $supply, $demand)
+    {
+        // 1. Initialize variables
+        $n = count($costs);
+        $m = count($costs[0]);
+        $u = $v = array_fill(0, $n + $m + 1, 0);
+        $pred = array_fill(0, $n + $m + 1, 0);
+        $x = array_fill(0, $n + $m + 1, 0);
+
+        // 2. Initialize the supply and demand variables
+        for ($i = 1; $i <= $n; $i++) {
+            $u[$i] = $supply[$i - 1];
+        }
+        for ($j = $n + 1; $j <= $n + $m; $j++) {
+            $v[$j] = $demand[$j - $n - 1];
+        }
+
+        // 3. Initialize the costs and pred arrays
+        for ($i = 1; $i <= $n; $i++) {
+            for ($j = $n + 1; $j <= $n + $m; $j++) {
+                $costs[$i][$j - $n - 1] += $u[$i] + $v[$j];
+                $pred[$j] = $i;
+            }
+        }
+
+        // 4. Find the initial feasible solution
+        $z = 0;
+        for ($j = $n + 1; $j <= $n + $m; $j++) {
+            $min = INF;
+            $k = 0;
+            for ($i = 1; $i <= $n; $i++) {
+                if ($costs[$i][$j - $n - 1] < $min) {
+                    $min = $costs[$i][$j - $n - 1];
+                    $k = $i;
+                }
+            }
+            $z += $min;
+            $x[$j] = $v[$j];
+            $v[$j] = 0;
+            if ($x[$j] > $u[$k]) {
+                $x[$pred[$j]] = $u[$k];
+                $v[$j] = $x[$j] - $u[$k];
+                $u[$k] = 0;
+            } else {
+                $x[$pred[$j]] = $x[$j];
+                $u[$k] -= $x[$j];
+            }
+        }
+
+        // 5. Iteratively improve the solution
+        while (true) {
+            // Find the non-basic variable with the smallest positive cost
+            $min = INF;
+            $k = 0;
+            for ($j = $n + 1; $j <= $n + $m; $j++) {
+                if ($costs[0][$j - $n - 1] < $min && $v[$j] > 0) {
+                    $min = $costs[0][$j - $n - 1];
+                    $k = $j;
+                }
+            }
+        }
+        return $costs;
     }
 }
